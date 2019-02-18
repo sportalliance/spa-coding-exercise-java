4084039b9c629619cf4557577f4a278768ebf6f3
Kjeld Peters <kjeld.peters@sportalliance.com>
2019-02-18 18:48:13 +0100

    first commit

---
+++ src/test/java/com/magicline/AppTest.java
@@ -0,0 +1,13 @@
+package com.magicline;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    public void testApp() {
+        assertEquals(1, 1);
+    }
+}
