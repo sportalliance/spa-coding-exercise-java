807d2eacb0f3e6623d7b73dce6c349b9dc5f2f39
Kai Meder <kai.meder@sportalliance.com>
2021-08-24 16:50:20 +0200

    add all-open/spring support

--- coding-exercise-kotlin/src/main/kotlin/spa/App.kt
+++ coding-exercise-kotlin/src/main/kotlin/spa/App.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class App
+class App
 
 fun main(args: Array<String>) {
 	SpringApplication.run(App::class.java, *args)
