251e48b43cdd3d1f0680753bb78b399a76c026e5
Kai Meder <kai.meder@sportalliance.com>
2021-08-24 16:30:35 +0200

    chore introduce kotlin

---
+++ coding-exercise-kotlin/src/main/kotlin/spa/App.kt
@@ -0,0 +1,12 @@
+package daidalos.invoicing
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class App
+
+fun main(args: Array<String>) {
+	SpringApplication.run(App::class.java, *args)
+}
+
