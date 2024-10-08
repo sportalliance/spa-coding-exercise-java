6c08ea7c9eb5f2e2454f65606a05f38cfb435a13
Lars Meynberg <lars.meynberg@sportalliance.com>
2024-10-08 08:29:44 +0200

    Update to latest spring boot and replace spring-fox with springdoc

--- coding-exercise-rest-jpa/src/main/java/spa/exercise/restjpa/RestJpaApplication.java
+++ coding-exercise-rest-jpa/src/main/java/spa/exercise/restjpa/RestJpaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Swagger UI is available at http://localhost:8080/swagger-ui/
+ * Swagger UI is available at <a href="http://localhost:8080/swagger-ui/index.html">http://localhost:8080/swagger-ui/index.html</a>
  */
 @SpringBootApplication
 public class RestJpaApplication {
